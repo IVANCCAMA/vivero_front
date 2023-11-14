13d872b0b6f61205cb7fb9d2e493658ddc5c38ba
Jhoselyn Ossio Mamani <ossiomjhossy@gmail.com>
2023-11-14 16:39:25 -0400

    Reportes parte 2

--- src/pages/Reportes.js
+++ src/pages/Reportes.js
@@ -33,17 +33,19 @@ const nInventario = [
     { name: 'Sin Inventario', value: 200 },
 ];
 const COLORS = [ '#25B651', '#FE8F02', '#FE0000'];
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+function renderCustomizedLabel({ cx, cy, midAngle, innerRadius, outerRadius, percent }) {
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const RADIAN = Math.PI / 180;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
     return (
         <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-        {`${(percent * 100).toFixed(0)}%`}
+            {`${(percent * 100).toFixed(0)}%`}
         </text>
-        );
-};
+    );
+}
+
 
 // GRAFICA DE DOS LINEAS DE LA ENTRADA Y SALIDA
 const reporteGraficas = [
@@ -64,7 +66,7 @@ const reporteGraficas = [
     //INDICADOR DE STOCK
         const RADIAN = Math.PI / 180;
             const indicadorStock = [
-                { name: 'A', value: 45, color: '#ff0000' },
+                { name: 'A', value: 45, color: '#97CDFC' },
                 { name: 'B', value: 45, color: '#00ff00' },
                 { name: 'C', value: 45, color: '#0000ff' },
                 { name: 'A', value: 45, color: '#ff0000' },
@@ -74,29 +76,62 @@ const reporteGraficas = [
             const iR = 50;
             const oR = 100;
             const value = 50;
-        const needle = (value, indicadorStock, cx, cy, iR, oR, color) => {
-            let total = 0;
+            const needle = (value, indicadorStock, cx, cy, iR, oR, color) => {
+                let total = 0;
                 indicadorStock.forEach((v) => {
-                    total += v.value;
+                  total += v.value;
                 });
-                    const ang = 180.0 * (1 - value / total);
-                    const length = (iR + 2 * oR) / 3;
-                    const sin = Math.sin(-RADIAN * ang);
-                    const cos = Math.cos(-RADIAN * ang);
-                    const r = 5;
-                    const x0 = cx + 5;
-                    const y0 = cy + 5;
-                    const xba = x0 + r * sin;
-                    const yba = y0 - r * cos;
-                    const xbb = x0 - r * sin;
-                    const ybb = y0 + r * cos;
-                    const xp = x0 + length * cos;
-                    const yp = y0 + length * sin;
-            return [
-                <circle cx={x0} cy={y0} r={r} fill={color} stroke="none" />,
-                <path d={`M${xba} ${yba}L${xbb} ${ybb} L${xp} ${yp} L${xba} ${yba}`} stroke="#none" fill={color} />,
-            ];
-        }
+                const elements = [];
+              
+                indicadorStock.forEach((entry, index) => {
+                  const ang = 180.0 * (1 - value / total);
+                  const midAngle = (ang / 2) * Math.PI / 180;
+              
+                  const labelRadius = iR + (oR - iR) * 0.5; // Adjust this radius for label positioning
+                  const x = cx + labelRadius * Math.cos(-midAngle);
+                  const y = cy + labelRadius * Math.sin(-midAngle);
+              
+                  // Agregar el texto en cada segmento
+                  elements.push(
+                    <text
+                      x={x}
+                      y={y}
+                      textAnchor={x > cx ? 'start' : 'end'}
+                      dominantBaseline="central"
+                      fill="white"
+                    >
+                      {entry.name}
+                    </text>
+                  );
+              
+                  // Segmento del indicador
+                  const angSlice = (entry.value / total) * ang;
+                  const length = (iR + 2 * oR) / 3;
+                  const sin = Math.sin(-RADIAN * angSlice);
+                  const cos = Math.cos(-RADIAN * angSlice);
+                  const r = 5;
+                  const x0 = cx + 5;
+                  const y0 = cy + 5;
+                  const xba = x0 + r * sin;
+                  const yba = y0 - r * cos;
+                  const xbb = x0 - r * sin;
+                  const ybb = y0 + r * cos;
+                  const xp = x0 + length * cos;
+                  const yp = y0 + length * sin;
+              
+                  // Circulo en el indicador
+                  elements.push(
+                    <circle cx={x0} cy={y0} r={r} fill={color} stroke="none" />
+                  );
+              
+                  // Trayectoria del indicador
+                  elements.push(
+                    <path d={`M${xba} ${yba}L${xbb} ${ybb} L${xp} ${yp} L${xba} ${yba}`} stroke="none" fill={color} />
+                  );
+                });
+              
+                return elements;
+              };        
 // GRAFICA EN LINEA SOLO UNO, UNIDADES DE STOCK
         const uStock = [
                 { name: 'Enero', uv: 0, pv: 0, amt: 0},
@@ -126,8 +161,10 @@ return (
                 <div className='graficas1'>
                     {/* GRAFICA EN BARRA DE LOS 5 PRODUCTOS DE  ENTRADAS Y SALIDAS */}
                 <div className='grafica-barra'>
-                <ResponsiveContainer width="100%" height={210}>
-                    <BarChart data={reporteBarra} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <p className='Letra-Reporte'>5 Productos top de entrada y salida</p>
+                <ResponsiveContainer width="100%" height={200}>
+                
+                    <BarChart data={reporteBarra} margin={{ top: 5, right: 10, left: -8, bottom: 5 }}>
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="product" />
                         <YAxis />
@@ -136,14 +173,15 @@ return (
                         <Bar dataKey="entrada" fill="#0000FE" />
                         <Bar dataKey="salida" fill="#97CDFC" />
                     </BarChart>
-                </ResponsiveContainer>
+                </ResponsiveContainer> 
                 </div>
                 
                 {/* GRAFICA EN TORTA DEL NIVEL DE INVENTARIO */}
                 <div className='grafica-torta'>
-                <ResponsiveContainer width="100%" height={200}>
+                    <p className='Letra-Reporte'>Nidel de inventario</p>
+                <ResponsiveContainer width="100%" height={165}>
                     <PieChart>
-                        <Pie
+                    <Pie
                         data={nInventario}
                         dataKey="value"
                         cx="50%"
@@ -151,35 +189,48 @@ return (
                         labelLine={false}
                         label={renderCustomizedLabel}
                         outerRadius={80}
-                        fill="#8884d8"
-                        >
+                        innerRadius={20}
+                        fill="white"
+                    >
                         {nInventario.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                         ))}
-                        </Pie>
+                    </Pie>
                     </PieChart>
                 </ResponsiveContainer>
+
+                {/* Leyenda */}
+                <div className='leyenda'>
+                    {nInventario.map((entry, index) => (
+                    <div key={`leyenda-${index}`} className='item-leyenda'>
+                        <div className='color-cuadrado' style={{ backgroundColor: COLORS[index] }}></div>
+                        <p>{entry.name}</p>
+                    </div>
+                    ))}
                 </div>
                 </div>
+                        </div>
                 <div className='graficas2'>
                     {/* GRAFICA DE DOS LINEAS DE LA ENTRADA Y SALIDA */}
                 <div className='grafica-linea2'>
+                <p>Total de entradas VS total de salidas</p>
                 <ResponsiveContainer width="100%" height={200}>
-                    <LineChart data={reporteGraficas} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <LineChart data={reporteGraficas} margin={{ top: 5, right: 10, left: 0, bottom: 5 }}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="name" />
                     <YAxis />
                     <Tooltip />
                     <Legend />
-                    <Line type="monotone" dataKey="Entrada" stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line type="monotone" dataKey="salida" stroke="#82ca9d" />
+                    <Line type="monotone" dataKey="Entrada" stroke="#0000FE" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="salida" stroke="#97CDFC" />
                 </LineChart>
                 </ResponsiveContainer>
                 </div>
-                <div className='grafica-Indicador'>
+                                    <div className='grafica-Indicador'>
+                    <p>Indicador de productos sin stock</p>
                     {/* INDICADOR DE STOCK */}
-                <PieChart width={400} height={200}>
-                    <Pie
+                    <PieChart width={400} height={200} className='indicador'>
+                        <Pie
                         dataKey="value"
                         startAngle={180}
                         endAngle={0}
@@ -190,16 +241,17 @@ return (
                         outerRadius={oR}
                         fill="#8884d8"
                         stroke="none"
-                    >
+                        >
                         {indicadorStock.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={entry.color} />
+                            <Cell key={`cell-${index}`} fill={entry.color} />
                         ))}
-                    </Pie>
-                    {needle(value, indicadorStock, cx, cy, iR, oR, '#d0d000')}
-                </PieChart>
-                </div>
+                        </Pie>
+                        {needle(value, indicadorStock, cx, cy, iR, oR, '#d0d000')}
+                    </PieChart>
+                    </div>
                 <div className='grafica-linea1'>
                     {/* GRAFICA EN LINEA SOLO UNO */}
+                    <p>Total de stock mensual</p>
                 <ResponsiveContainer width="100%" height={200}>
                     <AreaChart
                         width={500}
@@ -226,4 +278,5 @@ return (
     </div>
 );
 }
+
 export default Reportes;
