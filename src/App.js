0856307862ec679a5f414b2381ca4d6c24b4bea6
IVANCCAMA <ccamaivan@gmail.com>
2024-02-16 00:48:09 -0400

    Arreglar sobre esto

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Menu from './components/menu';
 import Home from './pages/Home';
-import Inventario from './pages/Inventario'; 
+import Inventario from './pages/Inventario';
 import Usuarios from './pages/Usuarios';
 import Transacciones from './pages/Transacciones';
 import Reportes from './pages/Reportes';
@@ -29,41 +29,47 @@ function App() {
     <AuthProvider>
       <div className='grid-container'>
         <Routes>
-          <Route path="/login" element={<Login />}/>
-            <Route
-              path="/*"
-              element={(
-                <>
-                  <Header />
-                  <Menu />
-                  <main className='main'>
-                    <Routes>
-                      <Route element={<PrivateRoutes />}>
-                        <Route path='/' exact element={<Home />} />
-                        <Route path="/usuarios" exact element={<Usuarios />} />
-                        <Route path="/inventario/producto" element={<Inventario />} />
-                        <Route path="/reportes" element={<Reportes />} />
-                        <Route path="/transacciones" element={<Transacciones />} />
-                        <Route path="/inventario/categoria" element={<Categoria />} />
-                        <Route path="/inventario/producto/formProducto" element={<FormProducto />} />
-                        <Route path="/inventario/categoria/formCategoria" element={<Formcategoria />} />
-                        <Route path="/inventario/categoria/editar/:id_categoria" element={<Formcategoria />} />
-                        <Route path="/inventario/categoria/editarCategoria/:id_categoria" element={<FormEditarCategoria />} />
-                        <Route path="/inventario/producto/editarProducto/:id_producto" element={<FormEditarProducto />} />
-                        <Route path="/inventario/producto/ver/:id_producto" element={<VerProducto />} />
-                        <Route path='/usuarios/crearUsuario' element={<FormUsuario />} />
-                        <Route path="/usuarios/editarUsuario/:id_usuario" element={<FormEditarUsuario />} />
-                        <Route path="/usuarios/ver/:id_usuario" element={<VerUsuario />} />
-                        <Route path='/transacciones/formTransacciones' element={<FormTransacciones/>}/>
-                        <Route path="/transacciones/ver/:id_transaccion" element={<VerTransaccion/>} />
-                      </Route>
-                      <Route path="/*" element={<NotFound />} />
-                    </Routes>
-                  </main>
-                  <Footer />
-                </>
-              )}
-            />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/*"
+            element={(
+              <>
+                <Header />
+                <div className='row'>
+                  <div className='col-2'>
+                    <Menu />
+                  </div>
+                  <div className='col-10'>
+                    <main className='main'>
+                      <Routes>
+                        <Route element={<PrivateRoutes />}>
+                          <Route path='/' exact element={<Home />} />
+                          <Route path="/usuarios" exact element={<Usuarios />} />
+                          <Route path="/inventario/producto" element={<Inventario />} />
+                          <Route path="/reportes" element={<Reportes />} />
+                          <Route path="/transacciones" element={<Transacciones />} />
+                          <Route path="/inventario/categoria" element={<Categoria />} />
+                          <Route path="/inventario/producto/formProducto" element={<FormProducto />} />
+                          <Route path="/inventario/categoria/formCategoria" element={<Formcategoria />} />
+                          <Route path="/inventario/categoria/editar/:id_categoria" element={<Formcategoria />} />
+                          <Route path="/inventario/categoria/editarCategoria/:id_categoria" element={<FormEditarCategoria />} />
+                          <Route path="/inventario/producto/editarProducto/:id_producto" element={<FormEditarProducto />} />
+                          <Route path="/inventario/producto/ver/:id_producto" element={<VerProducto />} />
+                          <Route path='/usuarios/crearUsuario' element={<FormUsuario />} />
+                          <Route path="/usuarios/editarUsuario/:id_usuario" element={<FormEditarUsuario />} />
+                          <Route path="/usuarios/ver/:id_usuario" element={<VerUsuario />} />
+                          <Route path='/transacciones/formTransacciones' element={<FormTransacciones />} />
+                          <Route path="/transacciones/ver/:id_transaccion" element={<VerTransaccion />} />
+                        </Route>
+                        <Route path="/*" element={<NotFound />} />
+                      </Routes>
+                    </main>
+                  </div>
+                </div>
+                <Footer />
+              </>
+            )}
+          />
         </Routes>
       </div>
     </AuthProvider>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import logo from '../img/logo.png';
-function Header(){
-    return(
+function Header() {
+    return (
         <header className='header'>
-    <div>
-    <img className='logo' src={logo} alt="Logo" />
-        <h4 className='texto'>SISTEMA GESTION DE INVENTARIO</h4>
-    </div>
-    </header>
+            <div>
+                <img className='logo' src={logo} alt="Logo" />
+                <h4 className='texto'>SISTEMA GESTION DE INVENTARIO</h4>
+            </div>
+        </header>
     )
 
 }
